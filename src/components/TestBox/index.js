c1ef7ad2ac738c4a845603dcd5d8bc793e5556a7
LucasAdorno <lucasadorno2702@gmail.com>
2020-07-21 22:57:34 -0300

    Complete Component

--- src/components/TestBox/index.js
+++ src/components/TestBox/index.js
@@ -1,61 +1,111 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Container } from './styles';
+import Button from '../Button';
+import Results from '../Results';
 
 const TestBox = (props) => {
-  const [min, setMin] = useState(0);
-  const [ddd, setDdd] = useState(0);
-  const [plan, setPlan] = useState(0);
-  const inputRef = useRef(null)
-  const dddRef = useRef(null)
-  const planRef = useRef(null)
-  const plans = [{ name: 'FaleMais 30', min: 30 }, { name: 'FaleMais 60', min: 60 }, { name: 'FaleMais 120', min: 120 }]
-  const values = {dsix: 1.90, dsev: 1.70, deig: 0.90}
+	const [min, setMin] = useState(0);
+	const [ddd, setDdd] = useState(0);
+	const [plan, setPlan] = useState(0);
+	const [prices, setPrices] = useState([0,0]);
+	const [results, setResults] = useState('-800px');
+	const inputRef = useRef(null)
+	const dddRef = useRef(null)
+	const planRef = useRef(null)
+	const plans = [{ name: 'FaleMais 30', min: 30 }, { name: 'FaleMais 60', min: 60 }, { name: 'FaleMais 120', min: 120 }]
+	const values = {dsix: 1.90, dsev: 1.70, deig: 0.90}
 
 
-  useEffect(()=>{
-    inputRef.current.addEventListener('keyup',(e) => {
-      setMin(parseInt(inputRef.current.value));
-    });
-    dddRef.current.addEventListener('click',(e) => {
-      setDdd(parseInt(dddRef.current.value));
-    });
-    planRef.current.addEventListener('click',(e) => {
-      setPlan(planRef.current.value);
-    });
-  }, [])
+	useEffect(()=>{
+		setDdd(parseInt(dddRef.current.value));
+		setPlan(planRef.current.value);
 
-  const handleClickButton = () => {
+		inputRef.current.addEventListener('keyup',(e) => {
+			setMin(Number(inputRef.current.value));
+		});
+		dddRef.current.addEventListener('change',(e) => {
+			setDdd(parseInt(dddRef.current.value));
+		});
+		planRef.current.addEventListener('change',(e) => {
+			setPlan(parseInt(planRef.current.value));
+		});
 
-     if( ddd === 16 ) {const fullPrice = min * values.dsix;
-     alert(fullPrice.toFixed(2))}
-     else if( ddd === 17 ) {const fullPrice = min * values.dsiv;
-     alert(fullPrice.toFixed(2))}
-     else if( ddd === 18 ) {const fullPrice = min * values.deig;
-     alert(fullPrice.toFixed(2))}
-  }
+	}, [])
+
+	const handleClickButton = () => {
+		let fullPrice;
+		let minExc = 0;
+		let planPrice;
+
+		if(min > plan){
+			minExc = min-plan;
+		}
+
+		if(ddd === 0 || min <= 0){
+			alert('Preencha os campos corretamente')
+		}
+
+		else{
+			if(props.defaultDdd === '11') {
+				if( ddd === 16 ) {
+					fullPrice = min * values.dsix;
+					planPrice = (minExc * values.dsix) + (minExc * values.dsix)/10;
+				}
+				else if( ddd === 17 ) {
+					fullPrice = min * values.dsev;
+					planPrice = (minExc * values.dsev) + (minExc * values.dsev)/10;
+				}
+				else if( ddd === 18 ) {
+					fullPrice = min * values.deig;
+					planPrice = (minExc * values.deig) + (minExc * values.deig)/10;
+				}
+			}
+
+			else if(props.defaultDdd === '16'){
+				fullPrice = min * (values.dsix+1);
+				planPrice = (minExc * values.dsix) + (minExc * values.dsix)/10;
+			}
+			else if(props.defaultDdd === '17'){
+				fullPrice = min * (values.dsev+1);
+				planPrice = (minExc * values.dsev) + (minExc * values.dsev)/10;
+			}
+			else if(props.defaultDdd === '18'){
+				fullPrice = min * (values.deig+1);
+				planPrice = (minExc * (values.deig+1)) + (minExc * (values.deig+1))/10;
+			}
+			setPrices([fullPrice, planPrice]);
+			setResults('0px')
+		}
+	}
 
 
-  return (
-    <Container className="content">
-      <h1>Teste agora um dos planos FaleMais!</h1>
-      <div className="testBox">
-        <label>ligar para:
-          <select ref={dddRef}>
-            {props.cities.map(i => <option value={i.ddd} key={i.cidade}>{i.cidade}</option>)}
-          </select>
-        </label>
-        <label>minutos:
-          <input type='number' ref={inputRef}/>
-        </label>
-        <label>plano FaleMais:
-          <select ref={planRef}>
-            {plans.map(i => <option key={i.min} value={i.min}>{i.name}</option>)}
-          </select>
-        </label>
-        <button onClick={()=>handleClickButton()}>Conferir</button>
-      </div>
-    </Container>
-  );
+
+	return (
+		<>
+		<Container className="content">
+			<h1>Teste agora um dos planos FaleMais!</h1>
+			<div className="testBox">
+				<h3>Veja qual plano<br/>   se adequa a você.</h3>
+				<label>ligar para:
+					<select ref={dddRef}>
+						<option value={0}>Selecione uma cidade</option>
+						{props.cities.map(i => <option value={i.ddd} key={i.cidade}>{i.cidade}</option>)}
+					</select>
+				</label>
+				<label>minutos:
+					<input type='number' ref={inputRef}/>
+				</label>
+				<label>plano FaleMais:
+					<select ref={planRef}>
+						{plans.map(i => <option key={i.min} value={i.min}>{i.name}</option>)}
+					</select>
+				</label>
+				<Button onClick={()=>handleClickButton()} text='Conferir' background={'#ffd53f'} />
+			</div>
+		</Container>
+		<Results onClick={()=>setResults('-800px')} visible={results} valor={prices} plan={plan} />
+		</>
+	);
 }
 
 export default TestBox;
--- src/components/TestBox/styles.js
+++ src/components/TestBox/styles.js
@@ -7,12 +7,21 @@ export const Container = styled.div`
   width:86%;
   margin: 30px auto 0;
 
-  h1 {
+  h1{
     width:56%;
     font-family: 'Expletus Sans', cursive;
     font-size: 92px;
     color: #fff;
     text-shadow: 2px 2px 4px #5554;
+  }
+
+  h3{
+    width:90%;
+    padding-bottom: 24px;
+    font-family: 'Expletus Sans', cursive;
+    font-size:30px;
+    color: #fff;
+    white-space: pre-wrap;
   }
 
   .testBox{
@@ -38,29 +47,25 @@ export const Container = styled.div`
   .testBox label select, .testBox label input{
     width: 263px;
     height: 45px;
+    padding-left: 4px;
     border: none;
     border-radius: 8px;
     background: #fafafa;
     outline: none;
   }
-
-  .testBox button {
-    width: 156px;
-    height: 43px;
-    border: none;
-    border-radius: 30px;
-    background: #ffd53f;
-    outline: none;
-    cursor: pointer;
+  .testBox label select:focus, .testBox label input:focus{
+    background: #eeeeee;
   }
+
 
   @media(max-width: 767px){
     flex-direction: column;
     align-items: flex-end;
+    margin-top: 4px;
 
     h1{
       width:100%;
-      font-size: 44px;
+      font-size: 32px;
     }
   }
 `;
